e97521b9d6420ca88dd82f77a53fcece86f4aca8
Dev Bhanushali <devbhanushali21@gmail.com>
2025-04-13 18:14:25 +0530

    some refactoring

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,17 +1,29 @@
-// config/db.js
+// config/db.js (Created by Bob - Commit 3)
 const mongoose = require('mongoose');
+// const config = require('./config'); // Or use process.env directly
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error('MongoDB Connection Error:', err.message);
-    process.exit(1); // Exit process with failure
-  }
+	try {
+		// const mongoURI = config.mongoURI || process.env.MONGO_URI;
+		const mongoURI = process.env.MONGO_URI; // Get URI from environment
+
+		if (!mongoURI) {
+			console.error('FATAL ERROR: MONGO_URI is not defined.');
+			process.exit(1);
+		}
+
+		await mongoose.connect(mongoURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			// Mongoose 6 doesn't need these anymore:
+			// useCreateIndex: true,
+			// useFindAndModify: false
+		});
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error('MongoDB Connection Error:', err.message);
+		process.exit(1); // Exit process with failure
+	}
 };
 
 module.exports = connectDB;
