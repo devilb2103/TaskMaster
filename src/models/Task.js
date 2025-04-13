f470ed30d1a1e8d437455a2b6de691e8d492b7a6
AbhishekSinha-git <abhishekksingh68@gmail.com>
2025-04-13 17:11:14 +0530

    Define Task model with Mongoose

---
+++ src/models/Task.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  // Owner will be added later
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
