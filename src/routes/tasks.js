9d28fe22cea2e1de9a566aba1a65628775b66637
AbhishekSinha-git <abhishekksingh68@gmail.com>
2025-04-13 17:39:43 +0530

    Protect Task creation route with auth middleware

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const Task = require('../models/Task');
+const protect = require('../middleware/auth'); 
 // Auth middleware will be added later
 
 const router = express.Router();
@@ -13,9 +14,10 @@ const router = express.Router();
 router.post(
   '/',
   [
+    protect,
     check('description', 'Description is required').not().isEmpty(),
   ],
-  async (req, res) => {
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -24,7 +26,7 @@ router.post(
     try {
       const newTask = new Task({
         description: req.body.description,
-        // owner: req.user.id // Will be added with auth
+        owner: req.user.id // Will be added with auth
       });
 
       const task = await newTask.save();
@@ -32,6 +34,7 @@ router.post(
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error while creating task');
+      next(err);
     }
   }
 );
